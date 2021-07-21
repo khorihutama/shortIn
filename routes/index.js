77c8bf32e4eb9fe7f6d452b71b7eeb3329fb51df
Khori Hutama <khori.qq@gmail.com>
2021-07-21 16:04:46 +0700

    feat: redirect root router to github

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Url = require('../models/Url')
 
+router.get('/', (req, res) => {
+    console.log('redirecting to github')
+    setTimeout(() => {
+        res.redirect("https://github.com/khorihutama/shortIn")
+    }, 3000);
+})
+
 router.get('/:urlId', async (req, res) => {
     try {
         const url = await Url.findOne({ urlId: req.params.urlId })
